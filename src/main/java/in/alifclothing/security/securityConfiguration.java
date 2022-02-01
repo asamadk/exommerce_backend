f9ca884d0c65d5884134993bbc7bbfbca642b6dd
Abdul samad Kirmani <71091738+asamadk@users.noreply.github.com>
2022-02-01 18:06:33 +0530

    Update securityConfiguration.java

--- src/main/java/in/alifclothing/security/securityConfiguration.java
+++ src/main/java/in/alifclothing/security/securityConfiguration.java
@@ -60,17 +60,5 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
                 antMatchers("/**").permitAll();
     }
     
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
 
 }
